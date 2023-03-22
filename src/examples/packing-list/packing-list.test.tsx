ad67b34481fcf422c64ebea2a79626ad04892525
Ben Zeise <ben.zeise@gmail.com>
2023-03-22 10:33:48 -0500

    resolving conflicts, working through test example

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,3 +1,4 @@
+import { userEvent } from '@testing-library/user-event/dist/types/setup';
 import { render, screen } from 'test/utilities';
 import PackingList from '.';
 
@@ -10,19 +11,52 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  expect.hasAssertions();
+  render(<PackingList />);
+  const newItemInput = screen.getByLabelText("New Item Name");
+  expect(newItemInput).toBeDefined();
+});
 
-it.todo(
+it(
   'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
+  () => {
+    render(<PackingList />);
+    const newItemInput = screen.getByLabelText("New Item Name");
+    const addNewItemButton = screen.getByRole('button', { name: 'Add New Item'});
+
+    expect(newItemInput).toHaveValue('');
+    expect(addNewItemButton).toBeDisabled();
+  },
 );
 
-it.todo(
+it(
   'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
+  async () => {
+    const {user} = render(<PackingList />);
+    const newItemInput = screen.getByLabelText("New Item Name");
+    const addNewItemButton = screen.getByRole('button', { name: 'Add New Item'});
+
+    await user.type(newItemInput, 'MackBoock Prokc');
+
+    expect(newItemInput).not.toHaveValue('');
+    expect(addNewItemButton).toBeEnabled();
+  },
 );
 
-it.todo(
+it(
   'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
+  async () => {
+    const {user} = render(<PackingList />);
+    const newItemInput = screen.getByLabelText("New Item Name");
+    const addNewItemButton = screen.getByRole('button', { name: 'Add New Item'});
+
+    await user.type(newItemInput, 'hglsiuhalsgMackBoock Prokc');
+
+    expect(screen.getByLabelText('hglsiuhalsgMackBoock Prokc')).toBeChecked();
+
+    await user.click(addNewItemButton);
+
+    expect(screen.getByLabelText('hglsiuhalsgMackBoock Prokc')).not.toBeChecked();
+  },
 );
